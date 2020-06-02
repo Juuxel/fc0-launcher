7235a9757819cb86c30a1059ec2becc5b4330d0e
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-06-02 12:58:37 +0300

    Logger: improve time formatting and add WARN
    It now only logs hour/minute/second and no nanoseconds.

--- src/main/java/juuxel/fc0/launcher/util/Logger.java
+++ src/main/java/juuxel/fc0/launcher/util/Logger.java
@@ -10,15 +10,21 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public final class Logger {
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH.mm.ss");
+
     private Logger() {
     }
 
     public static void info(String message) {
-        System.out.printf("%s [INFO] %s%n", LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME), message);
+        System.out.printf("%s [INFO] %s%n", LocalTime.now().format(TIME_FORMATTER), message);
     }
 
     public static void error(String message, Throwable error) {
-        System.err.printf("%s [ERROR] %s%n", LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME), message);
+        System.err.printf("%s [ERROR] %s%n", LocalTime.now().format(TIME_FORMATTER), message);
         error.printStackTrace();
+    }
+
+    public static void warn(String message) {
+        System.err.printf("%s [WARN] %s%n", LocalTime.now().format(TIME_FORMATTER), message);
     }
 }
